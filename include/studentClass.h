ac6a7f6baaad2da937212c64157ea0826c28d7e8
Yuntian Zhang <yuntian.zh@gmail.com>
2017-02-26 15:01:11 -0600

    Move file around and add .h for studentClass

--- include/studentClass.h
+++ include/studentClass.h
@@ -23,16 +23,7 @@ public:
 
 	//default constructor
 
-	Student()
-	{
-		firstName = "Jacob";
-		lastName = "Smith";
-		username = "smithj123";
-		studentID = 900341854;
-		classes.push_back("CS 3420");
-		classGrades.push_back("B");
-		GPA = 3;
-	}
+	Student();
 
 };
 #endif
---
+++ src/studentClass.cpp
@@ -0,0 +1,12 @@
+#include "studentClass.h"
+
+Student::Student()
+{
+	firstName = "Jacob";
+	lastName = "Smith";
+	username = "smithj123";
+	studentID = 900341854;
+	classes.push_back("CS 3420");
+	classGrades.push_back("B");
+	GPA = 3;
+}
