6e27b8650459a046077dbe8b5fc635db481c617c
Yuntian Zhang <yuntian.zh@gmail.com>
2017-02-26 16:51:42 -0600

    Rewrite adminLogin

--- include/adminLogin.h
+++ include/adminLogin.h
@@ -4,4 +4,7 @@
 
 //this header file just prototypes the function, the function definition is in adminLogin.cpp
 
+#define MAX_PASSWORD_ATTEMPT 3
+#define DEFAULT_ADMIN_PASSWORD "password"
+
 bool adminLogin();
--- src/adminLogin.cpp
+++ src/adminLogin.cpp
@@ -1,47 +1,31 @@
 #include "stdafx.h"
 
-//this cpp file has the adminLogin function in it
-
-string attemptedPassword;
-string password = "testpassword";	//this is what the password is
-int attemptedCount = 0;					//password attemptedCount
-
 using namespace std;
 
 bool adminLogin()
 {
-	system("cls");
-	cout <<"Type 'Exit' to go back to read-only menu" << endl;
-	cout << "attemptedCount left: " << 3 - attemptedCount << endl;
-	cout << "\nAdmin password? ";
 	cin.ignore();							//flushes the input stream so that getline starts fresh
-	getline(cin, attemptedPassword);
-
-											//testing to see if the password matches
-
-	if (attemptedPassword == "Exit SRMS")		//if they want to go back to the main menu
+	for (int i = 0; i < MAX_PASSWORD_ATTEMPT; i++)
 	{
-		return false;						//take them back
-	}
+		cout << "Please enter the password, or empty line to go back: ";
+		string password;
+		getline(cin, password);
 
-	else if (attemptedPassword == password)	//if they got the password correct
-	{
-		cout << "\nPassword correct! Press enter to go to full menu.";
-		cin.get();
-		return true;
-	}
+		//testing to see if the password matches
 
-	else
-	{
-		cout << "\nPassword incorrect! Press enter to try again.";
-		attemptedCount++;
-		cin.get();
-		if (attemptedCount == 3)		//if they've tried unsuccessfully to enter the password 3 times
+		if (password == "")		//if they want to go back to the main menu
 		{
-			return false;		//return them to the main menu with read-only access
+			return false;						//take them back
 		}
-		adminLogin();			//back to trying to enter password
-
+		else if (password == DEFAULT_ADMIN_PASSWORD)	//if they got the password correct
+		{
+			return true;
+		}
+		else
+		{
+			cout << "Incorrect password." << endl;
+		}
 	}
+	cout << "You have reached max retry count." << endl;
 	return false;
 }
