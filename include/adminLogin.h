eff213738bca95504c41a32cfcd9c0ef0097cb39
Yuntian Zhang <yuntian.zh@gmail.com>
2017-02-26 16:09:25 -0600

    Remove functions that are not in adminLogin.cpp

--- include/adminLogin.h
+++ include/adminLogin.h
@@ -5,8 +5,3 @@
 //this header file just prototypes the function, the function definition is in adminLogin.cpp
 
 bool adminLogin();
-void adminmenu();
-bool deleteStudent(const unsigned long studentID);
-bool modifyStudent(const unsigned long studentID);
-bool newStudent();
-Student searchStudent(const unsigned long studentID);
