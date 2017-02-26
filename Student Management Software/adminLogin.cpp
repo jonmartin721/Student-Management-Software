b243cd6b9c40e80aa9d6db8a5577158781bdfb45
Jonathan Martin <jonmartin721@gmail.com>
2017-02-26 14:31:30 -0600

    Renamed variables, passwordattempt - > passwordAttempt, attempts - > attemptedCount.

--- Student Management Software/adminLogin.cpp
+++ Student Management Software/adminLogin.cpp
@@ -6,9 +6,9 @@
 
 //this cpp file has the adminLogin function in it
 
-string passwordattempt;
+string attemptedPassword;
 string password = "testpassword";	//this is what the password is
-int attempts = 0;					//password attempts
+int attemptedCount = 0;					//password attemptedCount
 
 using namespace std;
 
@@ -16,19 +16,19 @@ bool adminLogin()
 {
 	system("cls");
 	cout <<"Type 'Exit' to go back to read-only menu" << endl;
-	cout << "Attempts left: " << 3 - attempts << endl;
+	cout << "attemptedCount left: " << 3 - attemptedCount << endl;
 	cout << "\nAdmin password? ";
 	cin.ignore();							//flushes the input stream so that getline starts fresh
-	getline(cin, passwordattempt);
+	getline(cin, attemptedPassword);
 
 											//testing to see if the password matches
 
-	if (passwordattempt == "Exit SRMS")		//if they want to go back to the main menu
+	if (attemptedPassword == "Exit SRMS")		//if they want to go back to the main menu
 	{
 		return false;						//take them back
 	}
 
-	else if (passwordattempt == password)	//if they got the password correct
+	else if (attemptedPassword == password)	//if they got the password correct
 	{
 		cout << "\nPassword correct! Press enter to go to full menu.";
 		cin.get();
@@ -38,9 +38,9 @@ bool adminLogin()
 	else
 	{
 		cout << "\nPassword incorrect! Press enter to try again.";
-		attempts++;
+		attemptedCount++;
 		cin.get();
-		if (attempts == 3)		//if they've tried unsuccessfully to enter the password 3 times
+		if (attemptedCount == 3)		//if they've tried unsuccessfully to enter the password 3 times
 		{
 			return false;		//return them to the main menu with read-only access
 		}
