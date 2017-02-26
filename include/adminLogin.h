8309a7c3d417775394db72cf6fc57aa41421e4b3
Yuntian Zhang <yuntian.zh@gmail.com>
2017-02-26 15:18:04 -0600

    Delete adminLogin() by accident

--- include/adminLogin.h
+++ include/adminLogin.h
@@ -5,6 +5,7 @@
 
 //this header file just prototypes the function, the function definition is in adminLogin.cpp
 
+bool adminLogin();
 void adminmenu();
 bool deleteStudent(const unsigned long studentID);
 bool modifyStudent(const unsigned long studentID);
