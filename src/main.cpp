4de043900ffd8ea909eb1401acdc17827d58f543
Yuntian Zhang <yuntian.zh@gmail.com>
2017-03-02 10:45:01 -0600

    Visual changes

--- src/main.cpp
+++ src/main.cpp
@@ -30,12 +30,14 @@ SIMS_OPERATION displayGuestMenu()
 	int option = 0;
 	while (true)
 	{
-		cout << "\nWelcome User, please choose an option." << endl;
-		cout << "\n1. Login as Admin" << endl;
+		cout << "Welcome User, please choose an option." << endl;
+		cout << endl;
+		cout << "1. Login as Admin" << endl;
 		cout << "2. Search for Student" << endl;
 		cout << "3. View all Student Records" << endl;
 		cout << "0. Exit" << endl;
-		cout << "\nChoice: ";
+		cout << endl;
+		cout << "Choice: ";
 		cin >> option;
 
 		switch (option)
@@ -59,15 +61,17 @@ SIMS_OPERATION displayAdminMenu()
 	int option = 0;
 	while (true)
 	{
-		cout << "\nWelcome Admin, please choose an option." << endl;
-		cout << "\n1. Logout of Admin" << endl;
+		cout << "Welcome Admin, please choose an option." << endl;
+		cout << endl;
+		cout << "1. Logout of Admin" << endl;
 		cout << "2. Search for Student" << endl;
 		cout << "3. View all Student Records" << endl;
 		cout << "4. Delete Student" << endl;
 		cout << "5. Modify Student" << endl;
 		cout << "6. New Student" << endl;
 		cout << "0. Exit SIMS" << endl;
-		cout << "\nChoice: ";
+		cout << endl;
+		cout << "Choice: ";
 		cin >> option;
 
 		switch (option)
@@ -101,11 +105,13 @@ SIMS_OPERATION displayMenu(LPSIMS_USER_TOKEN lpToken)
 	case USER:
 	{
 		cout << "[READ ONLY] - LOGIN AS ADMIN TO MODIFY RECORDS" << endl;
+		cout << endl;
 		return displayGuestMenu();
 	}
 	case ADMIN:
 	{
 		cout << "[FULL ACCESS] - LOGGED IN AS ADMIN" << endl;
+		cout << endl;
 		return displayAdminMenu();
 	}
 	default:
